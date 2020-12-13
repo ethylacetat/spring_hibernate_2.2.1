78ccff79f7d91dd5509deb701de918dc1a0b5aa3
ethylacetat <ethylacetat0@gmail.com>
2020-12-13 20:08:34 +0300

    feat: Car entity usage

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,12 +1,15 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
 public class MainApp {
    public static void main(String[] args) throws SQLException {
@@ -15,10 +18,20 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      Random rnd = new Random(System.nanoTime());
+
+      Car userCar;
+      userCar = new Car("Lada", rnd.nextInt(Integer.MAX_VALUE));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", userCar));
+
+      userCar = new Car("BMW", rnd.nextInt(Integer.MAX_VALUE));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", userCar));
+
+      userCar = new Car("Belaz", rnd.nextInt(Integer.MAX_VALUE));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", userCar));
+
+      userCar = new Car("Mini", rnd.nextInt(Integer.MAX_VALUE));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", userCar));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -27,6 +40,20 @@ public class MainApp {
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
          System.out.println();
+      }
+
+      System.out.println();
+
+      Optional<User> optionalUser = userService.getUserByCar(new Car("Mini", 832689763));
+      if (optionalUser.isPresent()) {
+         User user = optionalUser.get();
+         System.out.println("Id = " + user.getId());
+         System.out.println("First Name = " + user.getFirstName());
+         System.out.println("Last Name = " + user.getLastName());
+         System.out.println("Email = " + user.getEmail());
+         System.out.println("Car = " + user.getCar().getModel() + ":" + user.getCar().getSeries());
+      } else {
+         System.out.println("User not found");
       }
 
       context.close();
